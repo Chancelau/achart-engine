12e210e513ce2fb2126e2c4990121cf64dcd7d99
tiagotele@gmail.com <tiagotele@gmail.com@2f662ee2-5ab9-53b6-d772-61233dd46e2e>
2012-07-29 15:40:06 +0000

    Some comments to explain the code.

--- org.achartengine.chartdemo.demo.ChartDemo/src/org/achartengine/chartdemo/demo/chart/AverageCubicTemperatureChart.java
+++ org.achartengine.chartdemo.demo.ChartDemo/src/org/achartengine/chartdemo/demo/chart/AverageCubicTemperatureChart.java
@@ -57,20 +57,28 @@ public class AverageCubicTemperatureChart extends AbstractDemoChart {
    * @return the built intent
    */
   public Intent execute(Context context) {
+	  
     String[] titles = new String[] { "Crete", "Corfu", "Thassos", "Skiathos" };
+    
     List<double[]> x = new ArrayList<double[]>();
+    
     for (int i = 0; i < titles.length; i++) {
       x.add(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 });
     }
+    
     List<double[]> values = new ArrayList<double[]>();
+    
     values.add(new double[] { 12.3, 12.5, 13.8, 16.8, 20.4, 24.4, 26.4, 26.1, 23.6, 20.3, 17.2,
         13.9 });
     values.add(new double[] { 10, 10, 12, 15, 20, 24, 26, 26, 23, 18, 14, 11 });
     values.add(new double[] { 5, 5.3, 8, 12, 17, 22, 24.2, 24, 19, 15, 9, 6 });
     values.add(new double[] { 9, 10, 11, 15, 19, 23, 26, 25, 22, 18, 13, 10 });
+    
     int[] colors = new int[] { Color.BLUE, Color.GREEN, Color.CYAN, Color.YELLOW };
+    
     PointStyle[] styles = new PointStyle[] { PointStyle.CIRCLE, PointStyle.DIAMOND,
         PointStyle.TRIANGLE, PointStyle.SQUARE };
+    
     XYMultipleSeriesRenderer renderer = buildRenderer(colors, styles);
     int length = renderer.getSeriesRendererCount();
     for (int i = 0; i < length; i++) {
